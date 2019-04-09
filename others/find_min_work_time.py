9851c9ff509426159040e98578696f4250ac05a4
kioyoja07 <kiyoja07@gmail.com>
2019-04-09 16:10:34 +0900

    modified

--- others/find_min_work_time.py
+++ others/find_min_work_time.py
@@ -1,4 +1,6 @@
 
+# -*- coding: utf-8 -*-
+
 # def find_max_work_time(processor):
 #
 #     print('I have', processor, 'processor')
@@ -16,28 +18,11 @@
 #
 #     return max(works[0] + works[1], works[4])
 
+def check_input(num_works):
 
-def add_min_to_second(works):
+    """ 입력 조건이 맞는지 확인 """
 
-    """ 가장 작은 2개의 work를 한 개로 합치기"""
-
-    works = sorted(works)
-
-    if len(works) > 1:
-        new_work = works[0] + works[1]
-
-        del works[0]
-        del works[1]
-
-        works.append(new_work)
-
-        return works
-
-    else:
-        return works
-
-
-def find_max_work_time(processor, num_works):
+    print('you can assign only', num_works, 'works')
 
     while True:
 
@@ -46,19 +31,50 @@ def find_max_work_time(processor, num_works):
         works = [int(work) for work in works_input.split()]
 
         if len(works) == num_works:
-            break
 
-    while len(works) > processor:
+            return works
+
+
+def add_min_to_second(works):
+
+    """ 가장 작은 2개의 work를 한 개로 합치기 """
+
+    works = sorted(works)
+
+    if len(works) > 1:
+        added_work = works[0] + works[1]
+
+        del works[0]
+        del works[1]
+
+        works.append(added_work)
+
+        return works
+
+    else:
+        return works
+
+
+def find_total_work_time(works, processor):
+
+    """ 리스트를 줄여 나가면서 작업시간 계산"""
+
+    while len(works) > processor:     # 작업 묶음이 processor보다 크면 가장 짧은 작업 2개 합쳐서 작업 리스트 줄이기
 
         works = add_min_to_second(works)
 
-        if len(works) == processor:
-
-            return(max(works))
+    return(max(works))  # 작업 묶음이 processor 수와 같으면 각 processor의 최대 시간이 최종 작업 시간
 
 
 if __name__ == "__main__":
 
-    print(find_max_work_time(4, 5))
+    processor = 4
+    num_works = 5
+
+    works = check_input(num_works)  # 유효한 입력값을 리스트로 변환
+
+    total_work_time = find_total_work_time(works, processor)    # 총 작업 시간 구하기
+
+    print(total_work_time)
 
 
